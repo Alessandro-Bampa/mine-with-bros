ef31ca59c25a2b125ef1dd590bbff9e355d8055f
Alessandro Bampa <96387842+Alessandro-Bampa@users.noreply.github.com>
2024-02-01 17:58:15 +0100

    Update sync-server.bat

--- sync-server.bat
+++ sync-server.bat
@@ -2,17 +2,17 @@
 
 rem Questo file batch esegue il push dei file del server Minecraft su GitHub quando il server viene spento e aggiorna i file locali con l'ultima versione quando il server viene avviato.
 
-rem Controlla se il server è in esecuzione.
-if "%1" == "stop" (
+rem Update the repository with the server down
+if "%1" == "push" (
 
-rem Esegue il push dei file sul repository GitHub.
+rem Push files to the GitHub repository.
 git add .
 git commit -m " %DATE% %TIME% Sync server file"
 git push
 
 ) else (
 
-rem Aggiorna i file locali con l'ultima versione dal repository GitHub.
+rem Update local files with the latest version from the GitHub repository.
 git pull
 java -jar .\minecraft_server.1.20.4.jar --nogui
 )
